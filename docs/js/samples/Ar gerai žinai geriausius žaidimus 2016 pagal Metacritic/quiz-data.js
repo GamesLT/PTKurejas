3d2e321d7de4f038958d492ff730c8ceb4d20aa1
Raimondas Rimkevičius <github@mekdrop.name>
2017-04-23 13:37:09 +0300

    first sample for docs

---
+++ docs/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/quiz-data.js
@@ -0,0 +1,45 @@
+/**
+ * @CreatorName:		PTKūrėjas
+ * @CreatorVersion:	0.1.2.0
+ * @CreatorUrl:		https://github.com/Gameslt/PTKurejas
+ * @DateCreation:		2017-04-23 01:17
+ */
+
+var quiz_photo_data = ptkurejas_quiz(
+  {
+     "answers" : [
+		 'Axiom Verge',
+		 'Dark Souls III',
+		 'Forza Horizon 3',
+		 'GTA V',
+		 'Inside',
+		 'Keep Talking and Nobody Explodes',
+		 'Law Breakers',
+		 'NBA 2K17',
+		 'Need For Speed',
+		 'Ori and the Blind Forest: Definitive Edition',
+		 'Out of the Park Baseball 17',
+		 'Overwatch',
+		 'Pokemon Moon',
+		 'Quake Champions',
+		 'Rez Infinite',
+		 'Rocket League',
+		 'Stephen\'s Sausage Roll',
+		 'Titanfall 2',
+		 'Uncharted 4: A Thief\'s End',
+		 'Unreal Tournament'
+	 ] ,
+	 "images" : {
+		0 : '0.png',
+		1 : '1.png',
+		2 : '2.png',
+		4 : '3.png',
+		7 : '4.png',
+		10 : '5.png',
+		11 : '6.png',
+		14 : '7.png',
+		16 : '8.png',
+		18 : '9.png'
+	 }
+  }
+);
