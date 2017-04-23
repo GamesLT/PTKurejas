a76d0deda7a84e21efbed72d1f178715d704b54e
Raimondas Rimkevičius <github@mekdrop.name>
2017-04-24 00:39:25 +0300

    Update quiz-data.js

--- docs/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/quiz-data.js
+++ docs/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/quiz-data.js
@@ -30,16 +30,16 @@ var quiz_photo_data = ptkurejas_quiz(
 		 'Unreal Tournament'
 	 ] ,
 	 "images" : {
-		0 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/0.png',
-		1 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/1.png',
-		2 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/2.png',
-		4 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/3.png',
-		7 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/4.png',
-		10 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/5.png',
-		11 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/6.png',
-		14 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/7.png',
-		16 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/8.png',
-		18 : 'js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/9.png'
+		0 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/0.png',
+		1 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/1.png',
+		2 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/2.png',
+		4 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/3.png',
+		7 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/4.png',
+		10 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/5.png',
+		11 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/6.png',
+		14 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/7.png',
+		16 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/8.png',
+		18 : '//gameslt.github.io/PTKurejas/js/samples/Ar gerai žinai geriausius žaidimus 2016 pagal Metacritic/9.png'
 	 }
   }
 );
